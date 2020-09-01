b8f3de7f8b0aa9a856c713d15f98075d4219d321
Simon Sprankel <simon@customgento.com>
2020-09-01 14:29:23 +0200

    Remove compared widget output

---
+++ Plugin/RemoveCompareWidgetPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CustomGento\RemoveProductComparison\Plugin;
+
+class RemoveCompareWidgetPlugin
+{
+    public function afterRenderApp(): string
+    {
+        return '';
+    }
+}
